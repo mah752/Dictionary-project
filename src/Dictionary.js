3648eac93a99e90c07fa28fc2cc2e67de3975a3b
mah752 <83982869+mah752@users.noreply.github.com>
2022-04-10 18:09:44 +0100

    integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,27 +1,34 @@
-import React, {useState} from 'react'
-import "./dictionary.css"
+import React, {useState} from 'react';
+import axios from 'axios';
+import "./dictionary.css";
 
 const Dictionary = () => {
-     let [Keyword, setKeyword]= useState ("");
-     
-     function Search (event) {
-        event.preventDefault();
-        alert(`​🧙​ let me think of a defintion for "${Keyword}"`);
-    }
-    function handleKeywordChange(event){
-        //console.log (event.target.value)
-        setKeyword (event.target.value);
-    }
+  let [Keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+  //used https://dictionaryapi.dev/ to get dictionary API
+  function Search(event) {
+    event.preventDefault();
+    alert(`​🧙​ let me think of a defintion for "${Keyword}"`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${Keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeywordChange(event) {
+    //console.log (event.target.value)
+    setKeyword(event.target.value);
+  }
   return (
     <div>
-        <h2>Let's conjour up a definition for that word...</h2>
-        <div className="form">
-            <form onSubmit={Search}>
-            <input type="search" autofocus={true} onChange={handleKeywordChange}></input>
+      <h2>Let's conjour up a definition for that word...</h2>
+      <div className="form">
+        <form onSubmit={Search}>
+          <input type="search" onChange={handleKeywordChange}></input>
         </form>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Dictionary
