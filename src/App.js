af9191a4633c104b738c8f18e2979ae67435423e
mah752 <83982869+mah752@users.noreply.github.com>
2022-04-10 16:16:16 +0100

    deleted logo
    Deleted logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+      <header className="App-header">Hello</header>
     </div>
   );
 }
