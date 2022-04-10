fb9a4a936671240da7857b110dab8c93034ab401
mah752 <83982869+mah752@users.noreply.github.com>
2022-04-10 18:57:09 +0100

    Added lottie animation and imgs
    Working on styling animation and images. Also changed background colour

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Animation from "./Animation";
 import Dictionary from "./Dictionary";
+import diamonds from "./img/diamonds.png";
+import gem from "./img/gem.png"
 import "./App.css";
 
 
@@ -7,15 +9,17 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        <h1>Dictionary</h1>
+       <img className="diamonds" src={diamonds} alt="" /> 
+       <header className="App-header">
+          <h1>Dictionary</h1>
         </header>
-        <Animation/>
-      <main>
-        <Dictionary/>
-      </main>
+        <Animation />
+        <main>
+          <img className="gem" src={gem} alt="" />
+          <Dictionary />
+        </main>
+      </div>
     </div>
-  </div>
   );
 }
 
