2d6ae6e1ca87bb0567a9e2e4eaafe2248e3b30a8
mah752 <83982869+mah752@users.noreply.github.com>
2022-04-16 21:15:24 +0100

    Update Phonetic.js
    there are some phonetics that don’t contain audio. To remove them, changed bthe Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./Phonetic.css";
 import ReactAudioPlayer from "react-audio-player";
 
 const Phonetic = (props) => {
-  return (
-    <div className="Phonetic">
-      {props.phonetic.text}
-      <ReactAudioPlayer src={props.phonetic.audio} controls />
-    </div>
-  );
+  if (props.phonetic && props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        {props.phonetic.text}
+        <ReactAudioPlayer src={props.phonetic.audio} controls />
+      </div>
+    );
+  }
+  return null;
 };
 
-export default Phonetic
+export default Phonetic;
